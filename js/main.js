f20c38e510cd67f2ffa85fdf3c7671091cbc7ead
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2021-06-15 18:43:44 +0200

    Add arc function

--- js/main.js
+++ js/main.js
@@ -2020,7 +2020,167 @@ geojson.features.forEach(function (marker) {
     )
     .addTo(map);
 
-  el.addEventListener('mouseenter', () => mrk.togglePopup());
-  el.addEventListener('mouseleave', () => mrk.togglePopup());
 
+  // San Francisco
+  var origin = marker.geometry.coordinates;
+
+  // Washington DC
+  var destination = [-77.032, 38.913];
+
+  el.addEventListener('mouseenter', () => mapArc(origin, destination));
+  el.addEventListener('mouseleave', () => removeMapArc());
 });
+
+function removeMapArc() {
+  map.removeLayer("route");
+  map.removeSource("route");
+  map.removeLayer("point");
+  map.removeSource("point");
+}
+
+function mapArc(origin, destination) {
+  // A simple line from origin to destination
+  var route = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': [origin, destination]
+        }
+      }
+    ]
+  };
+
+  // A single point that animates along the route.
+  // Coordinates are initially set to origin.
+  var point = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'Point',
+          'coordinates': origin
+        }
+      }
+    ]
+  };
+
+  // Calculate the distance in kilometers between route start/end point.
+  var lineDistance = turf.length(route.features[0]);
+
+  var arc = [];
+
+  // Number of steps to use in the arc and animation, more steps means
+  // a smoother arc and animation, but too many steps will result in a
+  // low frame rate
+  var steps = 500;
+
+  // Draw an arc between the `origin` & `destination` of the two points
+  for (var i = 0; i < lineDistance; i += lineDistance / steps) {
+    var segment = turf.along(route.features[0], i);
+    arc.push(segment.geometry.coordinates);
+  }
+
+  // Update the route with calculated arc coordinates
+  route.features[0].geometry.coordinates = arc;
+
+  // Used to increment the value of the point measurement against the route.
+  var counter = 0;
+
+  map.addSource('route', {
+    'type': 'geojson',
+    'data': route
+    });
+     
+    map.addSource('point', {
+    'type': 'geojson',
+    'data': point
+    });
+     
+    map.addLayer({
+    'id': 'route',
+    'source': 'route',
+    'type': 'line',
+    'paint': {
+    'line-width': 2,
+    'line-color': '#007cbf'
+    }
+    });
+     
+    map.addLayer({
+    'id': 'point',
+    'source': 'point',
+    'type': 'symbol',
+    'layout': {
+    // This icon is a part of the Mapbox Streets style.
+    // To view all images available in a Mapbox style, open
+    // the style in Mapbox Studio and click the "Images" tab.
+    // To add a new image to the style at runtime see
+    // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+    'icon-image': 'airport-15',
+    'icon-rotate': ['get', 'bearing'],
+    'icon-rotation-alignment': 'map',
+    'icon-allow-overlap': true,
+    'icon-ignore-placement': true
+    }
+    });
+     
+    function animate() {
+    var start =
+    route.features[0].geometry.coordinates[
+    counter >= steps ? counter - 1 : counter
+    ];
+    var end =
+    route.features[0].geometry.coordinates[
+    counter >= steps ? counter : counter + 1
+    ];
+    if (!start || !end) return;
+     
+    // Update point geometry to a new position based on counter denoting
+    // the index to access the arc
+    point.features[0].geometry.coordinates =
+    route.features[0].geometry.coordinates[counter];
+     
+    // Calculate the bearing to ensure the icon is rotated to match the route arc
+    // The bearing is calculated between the current point and the next point, except
+    // at the end of the arc, which uses the previous point and the current point
+    point.features[0].properties.bearing = turf.bearing(
+    turf.point(start),
+    turf.point(end)
+    );
+     
+    // Update the source with this new data
+    map.getSource('point').setData(point);
+     
+    // Request the next frame of animation as long as the end has not been reached
+    if (counter < steps) {
+    requestAnimationFrame(animate);
+    }
+     
+    counter = counter + 1;
+    }
+     
+    document
+    .getElementById('replay')
+    .addEventListener('click', function () {
+    // Set the coordinates of the original point back to origin
+    point.features[0].geometry.coordinates = origin;
+     
+    // Update the source layer
+    map.getSource('point').setData(point);
+     
+    // Reset the counter
+    counter = 0;
+     
+    // Restart the animation
+    animate(counter);
+    });
+     
+    // Start the animation
+    animate(counter);
+}
+
