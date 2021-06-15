a8fa9c560baf9cbad35e9379ee2899786ce9195a
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2021-06-15 19:14:49 +0200

    Fix plane symbol

--- js/main.js
+++ js/main.js
@@ -2012,22 +2012,6 @@ function mapArc(origin, destination) {
     counter = counter + 1;
   }
 
-  document
-    .getElementById('replay')
-    .addEventListener('click', function () {
-      // Set the coordinates of the original point back to origin
-      point.features[0].geometry.coordinates = origin;
-
-      // Update the source layer
-      map.getSource('point').setData(point);
-
-      // Reset the counter
-      counter = 0;
-
-      // Restart the animation
-      animate(counter);
-    });
-
   // Start the animation
   animate(counter);
 }
